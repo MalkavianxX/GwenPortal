1e3356bcad7976af91c6a0403a6f7a770dbc60ff
MalkavianxX <mot46000@gmail.com>
2023-08-20 23:26:41 -0600

    blog update

--- inicio_sesion/admin.py
+++ inicio_sesion/admin.py
@@ -4,8 +4,7 @@ from .models import Comentario
 # Register your models here.
 class ComentarioAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'comentario', 'estatus', 'fecha', 'rate')
-    list_filter = ('estatus', 'fecha')
-    search_fields = ('usuario__username', 'comentario')
+
 
 # Registra el modelo Comentario utilizando la clase personalizada ComentarioAdmin
 admin.site.register(Comentario, ComentarioAdmin)
